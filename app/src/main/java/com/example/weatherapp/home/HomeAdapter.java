1c0791e38d8a431a966e2e5668c3b87520874b85
Patrik Strausz <patkolo1244@gmail.com>
2020-12-29 10:52:35 +0100

    One to one database

--- app/src/main/java/com/example/weatherapp/home/HomeAdapter.java
+++ app/src/main/java/com/example/weatherapp/home/HomeAdapter.java
@@ -8,25 +8,21 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.weatherapp.OnForecastClick;
 import com.example.weatherapp.R;
-import com.example.weatherapp.weather.WeatherResult;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.example.weatherapp.weather.WeatherResult;
 
 public class HomeAdapter extends RecyclerView.Adapter<HomeViewHolder> {
 
     private Context context;
 
-    private List<WeatherResult> mAllRepositories = new ArrayList<>();
+    private WeatherResult mAllRepositories;
 
 
-    public void setmAllRepositories(WeatherResult weatherResult) {
-        this.mAllRepositories.add(weatherResult);
+    public void setmAllRepositories(WeatherResult mAllRepositories) {
+        this.mAllRepositories = mAllRepositories;
         notifyDataSetChanged();
     }
-
 
 
     public HomeAdapter(Context context) {
@@ -39,25 +35,22 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeViewHolder> {
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.fragment_home_list, parent, false);
-
         return new HomeViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HomeViewHolder holder, int position) {
-        holder.bind(mAllRepositories.get(position), context);
+        holder.bind(mAllRepositories, context);
 
     }
 
     @Override
     public int getItemCount() {
-        if(mAllRepositories ==null){
+        if (mAllRepositories == null) {
             return 0;
-
-        }else{
-            return mAllRepositories.size();
+        } else {
+            return 1;
         }
-
     }
 
 
